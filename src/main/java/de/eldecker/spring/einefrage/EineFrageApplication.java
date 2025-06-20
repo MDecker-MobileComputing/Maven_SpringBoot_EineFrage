a1b5106cea5d2ea941c8e2be346a44d89d6652a8
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-06-20 17:26:49 +0200

    Projekt neu aufgesetzt
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.5.3&packaging=jar&jvmVersion=21&groupId=de.eldecker.spring&artifactId=einefrage&name=Eine%20Frage&description=Web-App%20f%C3%BCr%20Umfragen%20bestehend%20aus%20nur%20einer%20Frage&packageName=de.eldecker.spring.einefrage&dependencies=data-jpa,web,thymeleaf

---
+++ src/main/java/de/eldecker/spring/einefrage/EineFrageApplication.java
@@ -0,0 +1,18 @@
+package de.eldecker.spring.einefrage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Einstiegspunkt der Programmausführung.
+ */
+@SpringBootApplication
+public class EineFrageApplication {
+
+	public static void main( String[] args ) {
+        
+		SpringApplication.run( EineFrageApplication.class, args );
+	}
+
+}
---
+++ src/test/java/de/eldecker/spring/einefrage/EineFrageApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.spring.einefrage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EineFrageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
