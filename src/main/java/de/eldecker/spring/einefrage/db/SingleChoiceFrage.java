92f622285a2f78a29d3e6ebb5f3404332f465fd1
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-06-20 18:16:51 +0200

    Lade jetzt Demo-Daten

--- src/main/java/de/eldecker/spring/einefrage/db/SingleChoiceFrage.java
+++ src/main/java/de/eldecker/spring/einefrage/db/SingleChoiceFrage.java
@@ -2,10 +2,7 @@ package de.eldecker.spring.einefrage.db;
 
 import org.springframework.data.annotation.Version;
 
-import static jakarta.persistence.GenerationType.AUTO;
-
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -13,85 +10,93 @@ import jakarta.persistence.Table;
 @Table( name = "UMFRAGE" )
 public class SingleChoiceFrage {
 
+	/**
+	 * ID ist ein String, muss deshalb selbst gesetzt werden.
+	 */
     @Id
-    @GeneratedValue( strategy = AUTO )
-    private Long id;
-	
+    private String id;
+
     private String fragetext;
-    
-	private String antwort1text;    
-    private String antwort2text;    
-    private String antwort3text;    
+
+	private String antwort1text;
+    private String antwort2text;
+    private String antwort3text;
     private String antwort4text;
-    
+
     private int antwort1zaehler = 0;
     private int antwort2zaehler = 0;
     private int antwort3zaehler = 0;
     private int antwort4zaehler = 0;
-    
+
 	@Version
 	private Long version;
-	
-	
+
+
 	/**
 	 * Default-Konstruktor, obligatorisch für <i>Spring Data JPA</i>
 	 */
-	public SingleChoiceFrage() {}    
-	
-	public SingleChoiceFrage( String frageText,
+	public SingleChoiceFrage() {}
+
+	public SingleChoiceFrage( String id, String frageText,
 	                          String antwort1text, String antwort2text, String antwort3text, String antwort4text ) {
-		
+
+		this.id        = id;
 		this.fragetext = frageText;
-		
-		this.antwort1text = antwort1text; 
+
+		this.antwort1text = antwort1text;
 		this.antwort2text = antwort2text;
 		this.antwort3text = antwort3text;
 		this.antwort4text = antwort4text;
 	}
-    
-	
-	public Long getId() {
-		
+
+
+	public String getId() {
+
 		return id;
 	}
 	
+	public void setId( String id ) {
+
+		this.id = id; 
+	}	
+
     public String getFragetext() {
-    	
+
 		return fragetext;
 	}
 
 	public void setFragetext( String fragetext ) {
-		
+
 		this.fragetext = fragetext;
 	}
 
 
 	public String getAntwort1text() {
-		
+
 		return antwort1text;
 	}
 
 
 	public void setAntwort1text( String antwort1text ) {
-		
+
 		this.antwort1text = antwort1text;
 	}
 
 
 	public String getAntwort2text() {
-		
+
 		return antwort2text;
 	}
 
 
 	public void setAntwort2text( String antwort2text ) {
-		
+
 		this.antwort2text = antwort2text;
 	}
 
 
 	public String getAntwort3text() {
-		
+
 		return antwort3text;
 	}
 
@@ -102,25 +107,25 @@ public class SingleChoiceFrage {
 
 
 	public String getAntwort4text() {
-		
+
 		return antwort4text;
 	}
 
 
 	public void setAntwort4text( String antwort4text ) {
-		
+
 		this.antwort4text = antwort4text;
 	}
 
 
 	public int getAntwort1zaehler() {
-		
+
 		return antwort1zaehler;
 	}
 
 
 	public void setAntwort1zaehler(int antwort1zaehler) {
-		
+
 		this.antwort1zaehler = antwort1zaehler;
 	}
 
@@ -136,19 +141,19 @@ public class SingleChoiceFrage {
 
 
 	public int getAntwort3zaehler() {
-		
+
 		return antwort3zaehler;
 	}
 
 
 	public void setAntwort3zaehler( int antwort3zaehler ) {
-		
+
 		this.antwort3zaehler = antwort3zaehler;
 	}
 
 
 	public int getAntwort4zaehler() {
-		
+
 		return antwort4zaehler;
 	}
 
@@ -159,7 +164,7 @@ public class SingleChoiceFrage {
 
 
 	public Long getVersion() {
-		
+
 		return version;
 	}
 
---
+++ src/main/java/de/eldecker/spring/einefrage/logik/DemoDatenLader.java
@@ -0,0 +1,47 @@
+package de.eldecker.spring.einefrage.logik;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import de.eldecker.spring.einefrage.db.SingleChoiceFrage;
+import de.eldecker.spring.einefrage.db.SingleChoiceFrageRepo;
+
+
+@Component
+public class DemoDatenLader implements ApplicationRunner {
+
+	private static final Logger LOG = LoggerFactory.getLogger( DemoDatenLader.class );
+	
+	@Autowired
+	private SingleChoiceFrageRepo _frageRepo;
+	
+	@Override
+	public void run( ApplicationArguments args ) throws Exception {
+		
+		final long anzahlVorher = _frageRepo.count();
+		LOG.info( "Anzahl Fragen in DB: {}", anzahlVorher );
+		
+		if ( anzahlVorher > 0 ) {
+			
+			LOG.info( "Datenbank enthält schon Fragen, lade deshalb keine Demo-Fragen." );
+			
+		} else {
+			
+			LOG.info( "Datenbank ist leer, lade Demo-Fragen." );
+			
+			SingleChoiceFrage scf1 = new SingleChoiceFrage( "hunger",
+					                                        "Was soll es heute zum Mittagessen geben?", 
+					                                        "Schnitzel", "Sushi", "Schaschlik", "Salamipizza" );					                                        					                                        					                                        			
+			_frageRepo.save( scf1 );
+		}
+		
+		final long anzahlNachher = _frageRepo.count();
+		LOG.info( "Anzahl Fragen in DB: {}", anzahlNachher );		
+	}
+	
+}
