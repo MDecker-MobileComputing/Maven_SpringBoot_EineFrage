521df38517c6981bf89d5ed2993a71ab8aefb7c9
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-06-23 11:33:21 +0200

    Update JaNeinFrageEntity.java

--- src/main/java/de/eldecker/spring/einefrage/db/janein/JaNeinFrageEntity.java
+++ src/main/java/de/eldecker/spring/einefrage/db/janein/JaNeinFrageEntity.java
@@ -24,20 +24,20 @@ public class JaNeinFrageEntity {
     private String id;
 
     private String fragetext = "";
-    
-    private int zaehlerJa = 0;
+
+    private int zaehlerJa   = 0;
     private int zaehlerNein = 0;
-    
+
     private LocalDateTime zeitpunktErzeugung;
     private LocalDateTime zeitpunktLetzteAntwort;
-    
-    
+
+
     /**
      * Default-Konstruktor, obligatorisch für Spring Data JPA
      */
     public JaNeinFrageEntity() {
     }
-    
+
     public JaNeinFrageEntity( String id, String frageText ) {
 
         this.id        = id;
@@ -47,89 +47,89 @@ public class JaNeinFrageEntity {
     }
 
     public String getId() {
-        
+
         return id;
     }
 
     public String getFragetext() {
-        
+
         return fragetext;
     }
 
     public void setFragetext( String fragetext ) {
-        
+
         this.fragetext = fragetext;
     }
 
     public int getZaehlerJa() {
-        
+
         return zaehlerJa;
     }
 
     public double getZaehlerJaProzent() {
-        
+
         final double prozent = this.zaehlerJa * 100.0 / getGesamtzahlAntworten();
-        return nachkommastellenAbschneiden( prozent );  
+        return nachkommastellenAbschneiden( prozent );
     }
-    
+
     public void setZaehlerJa( int zaehlerJa ) {
-        
+
         this.zaehlerJa = zaehlerJa;
     }
 
     public int getZaehlerNein() {
-        
+
         return zaehlerNein;
     }
-    
+
     public double getZaehlerNeinProzent() {
-        
+
         final double prozent = this.zaehlerNein * 100.0 / getGesamtzahlAntworten();
-        return nachkommastellenAbschneiden( prozent );  
-    }    
+        return nachkommastellenAbschneiden( prozent );
+    }
 
     public void setZaehlerNein( int zaehlerNein ) {
-        
+
         this.zaehlerNein = zaehlerNein;
     }
 
     public LocalDateTime getZeitpunktErzeugung() {
-        
+
         return zeitpunktErzeugung;
     }
 
     public void setZeitpunktErzeugung( LocalDateTime zeitpunktErzeugung ) {
-        
+
         this.zeitpunktErzeugung = zeitpunktErzeugung;
     }
 
     public LocalDateTime getZeitpunktLetzteAntwort() {
-        
+
         return zeitpunktLetzteAntwort;
     }
 
     public void setZeitpunktLetzteAntwort( LocalDateTime zeitpunktLetzteAntwort ) {
-        
+
         this.zeitpunktLetzteAntwort = zeitpunktLetzteAntwort;
     }
 
     /**
      * Convenience-Methode, die die Summe der Ja- und Nein-Zähler zurückliefert.
-     * 
+     *
      * @return AnzahlJa + AnzahlNein
      */
     public int getGesamtzahlAntworten() {
 
         return zaehlerJa + zaehlerNein;
     }
-    
+
     /**
      * Convenience-Methode, die die Anzahl der Ja-Stimmen inkrementiert.
-     * 
+     *
      * @return Anzahl der Ja-Stimmen nach Inkrementierung
      */
     public int inkrementJa() {
-     
+
         zaehlerJa++;
         zeitpunktLetzteAntwort = now();
         return zaehlerJa;
@@ -137,26 +137,26 @@ public class JaNeinFrageEntity {
 
     /**
      * Convenience-Methode, die die Anzahl der Nein-Stimmen inkrementiert.
-     * 
+     *
      * @return Anzahl der Nein-Stimmen nach Inkrementierung
      */
     public int inkrementNein() {
-        
+
         zaehlerNein++;
         zeitpunktLetzteAntwort = now();
         return zaehlerNein;
     }
-    
+
     /**
      * String-Repräsentation des aufrufenden Objekts.
-     * 
+     *
      * @return String mit ID und Anzahl der Ja- und Nein-Stimmen
      */
     @Override
     public String toString() {
 
-        return format( "Ja-Nein-Frage mit ID=\"%s\", #Ja=%d, %Nein=%d.", 
+        return format( "Ja-Nein-Frage mit ID=\"%s\", #Ja=%d, %Nein=%d.",
                        id, zaehlerJa, zaehlerNein );
     }
-    
+
 }
